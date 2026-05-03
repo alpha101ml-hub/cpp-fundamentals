304e3b6609c5b5c61bbccacd78e7079a4decb850
alpha101ml-hub <alpha101ml@gmail.com>
2026-05-03 22:16:16 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int square(int n){
+    return n*n;
+}
+
+int main(){
+    square(5);
+    cout<<square(5)<<endl;
+}
---
+++ 3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter two integers: "<<endl;
+    cin>>a;
+    cin>>b;
+    cout<<"Sum: "<<a+b<<endl;
+}
