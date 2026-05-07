7a72c8cc887397e6c2e7ac6b8cd042388b7127be
Kratik Jarwal <alpha101ml@gmail.com>
2026-05-07 09:33:12 +0530

    Add files via upload

--- 8.cpp
+++ 8.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 using namespace std;
 
+// This program prints a square pattern of 1's and 0's
+// 1 0 0 0
+// 1 1 0 0
+// 1 1 1 0
+// 1 1 1 1
 int main(){
     for(int i=1; i<=4; i++){
         for(int j=1; j<=4; j++){
