416b044b5d4ebe7c034195b8d14d83588d30f67b
Kratik Jarwal <alpha101ml@gmail.com>
2026-06-07 00:44:57 +0530

    Add files via upload

---
+++ Sections/Section-9/Challenge/main.cpp
@@ -0,0 +1,48 @@
+// Section 9
+// Challenge
+/* 
+    This challenge is about using a collection (list) of integers and allowing the user to select options from a menu to perform operations on the list.
+
+    Your program should display a menu options to the user as follows:
+
+    P - Print numbers
+    A - Add a number
+    M - Display mean of the numbers
+    S - Display the smallest number
+    L - Display the largest number
+    Q - Quit
+
+    Enter your choice:
+
+The program should only accept valid choices from the user, both upper and lowercase selections should be allowed. If an illegal choice is made, you should display, "Unknown selection, please try again" and the menu options should be displayed again.
+
+If the user enters 'P' or 'p', you should display all the elements (ints) in the list.
+If the list is empty you should display "[] - the list is empty"
+If the list is not empty then all the lsit element should displayed inside square brackets separated by a space.
+For example, [ 1 2 3 4 5]
+    If the user enters 'M' or 'm', calculate the mean or average of the elements in the list and display it. 
+    If the list is empty, display "Unable to calculate mean - no data".
+
+    If the user enters 'S' or 's', display the smallest element in the list. 
+    For example, if the list contains [2 4 5 1], "The smallest number is 1".
+    If the list is empty, display "Unable to determine the smallest number - list is empty".
+
+    If the user enters 'L' or 'l', display the largest element in the list. 
+    For example, if the list contains [2 4 5 1], "The largest number is 5".
+    If the list is empty, display "Unable to determine the largest number - list is empty".
+
+    If the user enters 'Q' or 'q', then you should display "Goodbye" and the program should terminate.
+
+    Before you begin. Write out the steps you need to take and decide in what order they should be done.
+    Think about what loops you should use as well as what you will use for your selection logic.
+
+    Hint: Use a vector!
+
+    Additional fucntionaluty if you wish to extend this program
+
+    - search for a number in the list and if found display the number of tumes it occurs in the list
+    - clearing out the list (make it empty again) (Hint: the vector class has a .clear() method)
+    - don't allow duplicate entries
+    come up with your own ideas!
+*/
+
